58466e473657ca2397b20895c666fd5d40a44efd
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-02 16:54:55 -0700

    updated upload file function to support opening the file in the browser

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { formatUrl } from '@aws-sdk/util-format-url';
 import { Upload } from '@aws-sdk/lib-storage';
 import express from 'express';
 import multer from 'multer';
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 
 // Bringing in dotenv variables
 config();
@@ -14,11 +14,11 @@ const app = express();
 
 // AWS SDK Configuration
 const s3Client = new S3Client({
-  region: 'us-west-1',
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
+	region: 'us-west-1',
+	credentials: {
+		accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	},
 });
 
 // MIDDLEWARE: Handle File Uploads
@@ -26,38 +26,46 @@ const upload = multer();
 
 // POST: /upload-image
 app.post('/upload-image', upload.single('image'), async (req, res) => {
-  try {
-    const file = req.file;
-    const key = Date.now().toString() + '_' + file.originalname;
-    const bucketName = process.env.AWS_S3_BUCKET_NAME;
+	try {
+		const file = req.file;
+		const key = Date.now().toString() + '_' + file.originalname;
+		const bucketName = process.env.AWS_S3_BUCKET_NAME;
 
-    // Upload the file to S3
-    const upload = new Upload({
-      client: s3Client,
-      params: {
-        Bucket: bucketName,
-        Key: key,
-        Body: file.buffer,
-      },
-    });
-    await upload.done();
+		// Upload the file to S3
+		const upload = new Upload({
+			client: s3Client,
+			params: {
+				Bucket: bucketName,
+				Key: key,
+				Body: file.buffer,
+				ContentType: file.mimetype,
+				ContentDisposition: 'inline',
+			},
+		});
+		await upload.done();
 
-    // Generate a pre-signed URL for the uploaded file
-    const presigner = new S3RequestPresigner(s3Client.config);
-    const getObjectCommand = new PutObjectCommand({ Bucket: bucketName, Key: key });
-    const request = await createRequest(s3Client, getObjectCommand);
-    const imageUrl = formatUrl(request, presigner);
+		// Generate a pre-signed URL for the uploaded file
+		const presigner = new S3RequestPresigner(s3Client.config);
+		const getObjectCommand = new PutObjectCommand({
+			Bucket: bucketName,
+			Key: key,
+		});
+		const request = await createRequest(s3Client, getObjectCommand);
+		const imageUrl = formatUrl(request, presigner);
 
-    res.send({
-      message: 'Image uploaded successfully',
-      imageUrl: imageUrl,
-    });
-  } catch (error) {
-    res.status(500).send({ message: 'An error occurred while uploading the image', error: error.message });
-  }
+		res.send({
+			message: 'Image uploaded successfully',
+			imageUrl: imageUrl,
+		});
+	} catch (error) {
+		res.status(500).send({
+			message: 'An error occurred while uploading the image',
+			error: error.message,
+		});
+	}
 });
 
 // Start the Express server
-app.listen(3000, () => {
-  console.log('Express server is running on port 3000');
+app.listen(3001, () => {
+	console.log('Express server is running on port 3001');
 });
