734ee0ff453dd5276227e98f9e7e4eb29bec4098
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-03 16:41:23 -0700

    finished the user model

--- models/user.js
+++ models/user.js
@@ -1,77 +1,224 @@
-"use strict";
-import { LicenseManagerUserSubscriptions } from "aws-sdk";
-import db from "../db";
-import bcrpyt from "bcrypt";
-import { BCRYPT_WORK_FACTOR } from "../config";
-import { BadRequestError, UnauthorizedError } from "../expressError";
+'use strict';
+import db from '../db';
+import bcrypt from 'bcrypt';
+import { BCRYPT_WORK_FACTOR } from '../config';
+import { BadRequestError, UnauthorizedError } from '../expressError';
+import { sqlForPartialUpdate } from '../helpers/sql';
 
 class User {
-  constructor({ username, password, firstName, lastName, email }) {
-    this.username = username;
-    this.password = password;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.email = email;
-  }
-
-  static async authenticate(username, password) {
-    // try to find the user first
-    const result = await db.query(
-      `
-        SELECT username,
+	/** authenticate user with username, password.
+	 *
+	 * Returns { id, username, first_name, last_name, email}
+	 *
+	 * Throws UnauthorizedError is user not found or wrong password.
+	 **/
+	static async authenticate(username, password) {
+		// try to find the user first
+		const result = await db.query(
+			`
+        SELECT id,
+               username,
                password,
                first_name AS "firstName",
                last_name  AS "lastName",
-               email,
-               is_admin   AS "isAdmin"
+               email
         FROM users
         WHERE username = $1`,
-      [username]
-    );
+			[username]
+		);
 
-    const user = result.rows[0];
+		const user = result.rows[0];
 
-    if (user) {
-      // compare hashed password to a new hash from password
-      const isValid = await bcrypt.compare(password, user.password);
-      if (isValid === true) {
-        delete user.password;
-        return user;
-      }
-    }
+		if (user) {
+			// compare hashed password to a new hash from password
+			const isValid = await bcrypt.compare(password, user.password);
+			if (isValid === true) {
+				delete user.password;
+				return user;
+			}
+		}
 
-    throw new UnauthorizedError("Invalid username/password");
-  }
+		throw new UnauthorizedError('Invalid username/password');
+	}
 
-  static async getUser({ username }) {
-    const result = await db.query(
-      `
-    select username,
-          password,
-          first_name as firstName,
-          last_name as lastName,
-          email
-    from users
-    where username = $1`,
-      [username]
-    );
-    validUser = new User(result.rows[0]);
+	/** Register user with data.
+	 *
+	 * Returns { username, firstName, lastName, email }
+	 *
+	 * Throws BadRequestError on duplicates.
+	 **/
+	static async register({ username, password, firstName, lastName, email }) {
+		const duplicateCheck = await db.query(
+			`
+      SELECT username
+      FROM users
+      WHERE username = $1`,
+			[username]
+		);
 
-    return validUser;
-  }
+		if (duplicateCheck.rows.length > 0) {
+			throw new BadRequestError(`Duplicate username: ${username}`);
+		}
 
-  static async getAll() {
-    const result = await db.query(
-      `
-    select username,
-          password,
-          first_name as firstName,
-          last_name as lastName,
-          email
-    from users
+		const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+
+		const result = await db.query(
+			`
+              INSERT INTO users
+              (username,
+               password,
+               first_name,
+               last_name,
+               email)
+              VALUES ($1, $2, $3, $4, $5)
+              RETURNING
+                  id,
+                  username,
+                  first_name AS "firstName",
+                  last_name AS "lastName",
+                  email`,
+			[username, hashedPassword, firstName, lastName, email]
+		);
+
+		const user = result.rows[0];
+
+		return user;
+	}
+
+	/** Find all users.
+	 *
+	 * Returns [{ id, username, first_name, last_name, email, is_admin }, ...]
+	 **/
+
+	static async findAll() {
+		const result = await db.query(
+			`
+    SELECT id,
+           username,
+           password,
+           first_name as firstName,
+           last_name as lastName,
+           email
+      FROM users
+  ORDER BY username
     `
-    );
-    return result.rows;
-  }
+		);
+		return result.rows;
+	}
+
+	/** Given a username, return data about user.
+	 *
+	 * Returns { id, username, firstName, lastName, listings, bookings, conversations }
+	 *   where listings is [{ id, name, description, price, street, city, state, zip, genre }]
+	 *   where reservations is [{ id, owner_id, renter_id, listing_id, created_at }]
+	 *   where conversations is [{ id, renter_id, owner_id, listing_id }]
+	 *
+	 * Throws NotFoundError if user not found.
+	 **/
+
+	static async getUser({ id }) {
+		const result = await db.query(
+			` SELECT id,
+               username,
+               first_name as firstName,
+               last_name as lastName,
+               email
+          FROM users
+         WHERE id = $1`,
+			[id]
+		);
+		const user = result.rows[0];
+
+		if (!user) throw new NotFoundError(`User not found: ${id}`);
+
+		const listings = await db.query(
+			`
+        SELECT id, name, description, price, street, city, state, zip, genre
+        FROM listings
+        WHERE owner_id = $1
+        ORDER BY id`,
+			[id]
+		);
+
+		user.listings = listings.rows;
+
+		const reservations = await db.query(
+			`
+        SELECT id, owner_id, renter_id, listing_id, created_at
+        FROM bookings
+        WHERE renter_id = $1
+        ORDER BY id`,
+			[id]
+		);
+
+		user.reservations = reservations.rows;
+
+		const conversations = await db.query(
+			`
+        SELECT id, renter_id, owner_id, listing_id
+        FROM conversations
+        WHERE owner_id = $1 OR renter_id = $1
+        ORDER BY id`,
+			[id]
+		);
+
+		user.conversations = conversations.rows;
+
+		return company;
+	}
+
+	/** Update user data with `data`. (partial update)
+	 *
+	 * Data can include:
+	 *   { firstName, lastName, password, email }
+	 *
+	 * Returns { id, username, firstName, lastName, email }
+	 *
+	 * Throws NotFoundError if not found.
+	 *
+	 */
+
+	static async update(id, data) {
+		if (data.password) {
+			data.password = await bcrypt.hash(data.password, BCRYPT_WORK_FACTOR);
+		}
+
+		const { setCols, values } = sqlForPartialUpdate(data, {
+			firstName: 'first_name',
+			lastName: 'last_name',
+		});
+		const idVarIdx = '$' + (values.length + 1);
+
+		const querySql = `
+      UPDATE users
+      SET ${setCols}
+      WHERE id = ${idVarIdx}
+      RETURNING id,
+          first_name AS "firstName",
+          last_name AS "lastName",
+          email`;
+		const result = await db.query(querySql, [...values, id]);
+		const user = result.rows[0];
+
+		if (!user) throw new NotFoundError(`No user: ${id}`);
+
+		delete user.password;
+		return user;
+	}
+	/** Delete given user from database; returns undefined. */
+
+	static async remove(id) {
+		let result = await db.query(
+			`
+        DELETE
+        FROM users
+        WHERE id = $1
+        RETURNING id`,
+			[id]
+		);
+		const user = result.rows[0];
+
+		if (!user) throw new NotFoundError(`No user: ${id}`);
+	}
 }
 export default User;
