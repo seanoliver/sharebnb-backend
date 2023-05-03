16e7118a9374da09febf823a8f881f47324a7af8
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-03 12:32:28 -0700

    started work on listing model

--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,22 @@
-"use strict";
-class Listing{
-
+'use strict';
+class Listing {
+	static async create(data) {
+		const result = await db.query(
+			`
+    INSERT INTO jobs (name,
+                      description,
+                      price,
+                      address)
+    VALUES ($1, $2, $3, $4)
+    RETURNING
+        id,
+        title,
+        salary,
+        equity,
+        company_handle AS "companyHandle"`,
+			[data.title, data.salary, data.equity, data.companyHandle]
+		);
+	}
 }
 
 export default Listing;
