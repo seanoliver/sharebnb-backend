eb8327f3e96e0feb7ea3aebe719317ec70c3519f
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-05-04 23:02:42 -0700

    formatting

--- routes/auth.js
+++ routes/auth.js
@@ -6,12 +6,13 @@ const jsonschema = require("jsonschema");
 
 const User = require("../models/user");
 const express = require("express");
-const router = new express.Router();
 const { createToken } = require("../helpers/tokens");
 const userAuthSchema = require("../schemas/userAuth.json");
 const userRegisterSchema = require("../schemas/userRegister.json");
 const { BadRequestError } = require("../expressError");
 
+// Initialize router
+const router = new express.Router();
 
 /** POST /auth/token:  { username, password } => { token }
  *
